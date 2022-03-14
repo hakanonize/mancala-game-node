a2f822ddb6b7b9afc79475c136113bd70ceb025f
Hakan <hakanonize35@gmail.com>
2022-03-14 12:31:36 +0300

    added production client serve

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { createServer } from 'http';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { handleError } from './middlewares/index.js';
 import { config } from './config/index.js';
@@ -12,6 +14,15 @@ const socketServer = createServer(app);
 socket(socketServer);
 app.use(cors());
 app.use(express.json());
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, '../../client', 'build')));
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '../../client', 'build', 'index.html'));
+});
 
 app.use((error, _request, response, _) => {
   handleError(error, response);
